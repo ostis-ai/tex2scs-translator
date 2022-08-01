268404d514dc7e2d78c7afbe907ccd68f839b779
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-08-01 11:35:55 +0300

    feat: config translator

--- src/sc_scn_tex2scs_translator_builder.h
+++ src/sc_scn_tex2scs_translator_builder.h
@@ -2,17 +2,13 @@
 
 #include "memory"
 #include "translator/sc_scn_tex2scs_translator.h"
-#include "translator/commands/sc_scn_tex_idtf_command_defines.h"
+#include "translator/commands/sc_scn_tex_command_defines.h"
 
 class ScSCnTex2SCsTranslatorBuilder
 {
 public:
   static std::unique_ptr<ScSCnTex2SCsTranslator> BuildDefaultTranslator()
   {
-    ScSCnTexCommands const & commands = {
-        { "scnidtf", new ScSCnTexIdtfCommand() },
-    };
-
-    return std::make_unique<ScSCnTex2SCsTranslator>(commands);
+    return std::make_unique<ScSCnTex2SCsTranslator>();
   }
 };
