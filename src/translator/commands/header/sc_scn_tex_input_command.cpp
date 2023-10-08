f89e3729e48fb09ca5bd39a6ec933d17e8d3ee64
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-10-08 14:07:12 +0300

    refactor: hpp and cpp files

---
+++ src/translator/commands/header/sc_scn_tex_input_command.cpp
@@ -0,0 +1,12 @@
+#include "sc_scn_tex_input_command.h"
+
+#include "../../helper/scs_helper.h"
+
+ScScnTexCommandResult ScSCnTexInputCommand::Complete(
+    ScSCnCommandsHistory & history,
+    ScSCnPrefixTree & tree,
+    ScScnTexCommandParams const & params)
+{
+  std::string const & path = params.at(1);
+
+}
--- src/translator/tree/sc_scn_prefix_tree.h
+++ src/translator/tree/sc_scn_prefix_tree.h
@@ -9,50 +9,13 @@
 class ScSCnPrefixTree
 {
 public:
-  static ScSCnPrefixTree * GetInstance()
-  {
-    if (m_instance == nullptr)
-      m_instance = new ScSCnPrefixTree();
+  static ScSCnPrefixTree * GetInstance();
 
-    return m_instance;
-  }
+  std::string Add(std::string const & key, std::string const & nodeType);
 
-  std::string Add(std::string const & key, std::string const & nodeType)
-  {
-    auto const & item = m_translations.find(key);
-    if (item != m_translations.end())
-      return item->second.first;
+  std::string Get(std::string const & key);
 
-    std::string const & value = ".system_element_" + std::to_string(index);
-    m_translations.insert({ key, { value, nodeType } });
-
-    ++index;
-
-    return value;
-  }
-
-  std::string Get(std::string const & key)
-  {
-    auto const & item = m_translations.find(key);
-    if (item != m_translations.end())
-      return item->second.first;
-
-    return "";
-  }
-
-  std::string Dump() const
-  {
-    SCsStream stream;
-    for (auto const & item : m_translations)
-    {
-      stream.Row([&item]() -> SCsStream {
-        return { item.second.first, " => nrel_main_idtf: [", item.first, "] "
-          "(* <- lang_ru;; => nrel_format: format_html;; *);;\n",
-          item.second.first, " <- ", item.second.second, ";;\n\n" };
-      });
-    }
-    return stream;
-  }
+  std::string Dump() const;
 
 protected:
   static ScSCnPrefixTree * m_instance;
